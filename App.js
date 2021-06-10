7ed3c14c9e4354cb0836c3d9aa1024c1f8888749
UzairSammad <uzair.iu17@gmail.com>
2021-06-10 16:57:34 +0500

    implemented face book login

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 import auth from '@react-native-firebase/auth';
 import React from 'react';
 import { Button, View, Text } from 'react-native';
+import { LoginManager, AccessToken } from 'react-native-fbsdk';
+
 
 
 
@@ -18,12 +20,46 @@ const App = () => {
     // Sign-in the user with the credential
     return auth().signInWithCredential(googleCredential);
   }
+
+  async function onFacebookButtonPress() {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+  
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+  
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+  
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+  
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+    alert(JSON.stringify(facebookCredential))
+  
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
+  }
+
   function GoogleSignIn() {
     return (
+      
       <Button
         title="Google Sign-In"
        
         onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!')).catch(error=>console.log(error))}
+      />
+    );
+  }
+
+  function FacebookSignIn() {
+    return (
+      <Button
+        title="Facebook Sign-In"
+        onPress={() => onFacebookButtonPress().then(() => console.log('Signed in with Facebook!'))}
       />
     );
   }
@@ -42,6 +78,10 @@ const App = () => {
       <View style={{ flex: 0.8,  }}>
         {/* <Text style={{ fontSize: 18 }}>Welcome to Social Login Application</Text> */}
         {GoogleSignIn()}
+        <View style={{marginTop:20}}>
+
+        {FacebookSignIn()}
+        </View>
       </View>
     </View>
   )
